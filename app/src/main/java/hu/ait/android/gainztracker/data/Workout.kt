9801f7babc4639135d17361a35931877f4ec6465
Sawyer Pruett <sapruett@davidson.edu>
2018-12-10 20:12:05 +0100

    added array of exercises to workout data class

--- app/src/main/java/hu/ait/android/gainztracker/data/Workout.kt
+++ app/src/main/java/hu/ait/android/gainztracker/data/Workout.kt
@@ -1,4 +1,5 @@
 package hu.ait.android.gainztracker.data
 
 data class Workout(var name: String = "",
-                var type: String = "")
+                var type: String = "",
+                   var exercises: Array<Exercise>)
