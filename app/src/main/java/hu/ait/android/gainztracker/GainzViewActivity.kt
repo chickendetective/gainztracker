8d1498b770760ce43f723e3457bd1680d5f90c08
Sawyer Pruett <sapruett@davidson.edu>
2018-12-16 22:25:01 +0100

    fixed gainzview

--- app/src/main/java/hu/ait/android/gainztracker/GainzViewActivity.kt
+++ app/src/main/java/hu/ait/android/gainztracker/GainzViewActivity.kt
@@ -3,6 +3,8 @@ package hu.ait.android.gainztracker
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.bumptech.glide.Glide
 import hu.ait.android.gainztracker.R.drawable.pre_gainz_pic
 import kotlinx.android.synthetic.main.activity_gainz_viewer.*
 import java.net.URL
@@ -12,11 +14,10 @@ class GainzViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gainz_viewer)
-        lateinit var imgUrl: URL
+        lateinit var imgUrl: String
         if (intent.hasExtra(DateActivity.KEY_VIEW_GAINZ)) {
-            imgUrl = URL(intent.getStringExtra(DateActivity.KEY_VIEW_GAINZ))
+            imgUrl = intent.getStringExtra(DateActivity.KEY_VIEW_GAINZ)
         }
-        val bitmap = BitmapFactory.decodeStream(imgUrl.openConnection().getInputStream())
-        ivViewGainz.setImageBitmap(bitmap)
+        Glide.with(applicationContext).load(imgUrl).into(ivViewGainz)
     }
 }
